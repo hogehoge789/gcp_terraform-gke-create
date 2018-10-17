05d02f8243807a4f0508a7af6c3e2b2de6d4013b
hogehoge789 <hogehoge789@gmail.com>
2018-10-17 23:29:25 +0900

    commit

--- compute_instance.tf
+++ compute_instance.tf
@@ -1,4 +1,23 @@
 provider "google" {
-	project = "My First Project"
+	project = "zeta-arena-185314"
 	region  = "asia-northeast1"
   }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config = {
+    }
+  }
+}
